e2b01bd9796e59332772e4b8c2f0f08b56b87b63
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-11-30 00:14:14 +0200

    Add files via upload

---
+++ 3.0 Conditional Statements Advanced - Lab/01-dayOfWeek.py
@@ -0,0 +1,22 @@
+#dayOfWeek
+def fun(arg):
+    switcher = {
+          1: "Monday",
+          2: "Tuesday",
+          3: "Wednesday",
+          4: "Thursday",
+          5: "Friday",
+          6: "Saturday",
+          7: "Sunday"
+    }
+    return switcher.get(arg, "Error")
+
+print(fun(int(input())))
+#7 //Sunday
+
+
+
+
+
+
+
---
+++ 3.0 Conditional Statements Advanced - Lab/10-invalidNubmer.py
@@ -0,0 +1,4 @@
+n = float(input())
+print('invalid' if not (100 <= n and not n > 200 or n == 0) else '')
+
+#0 //0
